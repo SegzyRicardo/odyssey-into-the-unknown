a638afa48ff7239cf1c87c691c8fb66cc01485ce
SegzyRicardo <risko272@gmail.com>
2021-03-12 11:45:06 +0100

    Time Changed

---
+++ Odyssey into the unknown/Assets/Scripts/Leg_Move.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leg_Move : MonoBehaviour
+{
+    public Transform RayStart;
+
+    public LayerMask Layer;
+
+    public float MaxLenght = 20;
+
+    [Header("Test Parameters")]
+
+    public Transform HitMark;
+    /*void Start()
+    {
+
+    }*/
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+
+
+    }
+}
+
